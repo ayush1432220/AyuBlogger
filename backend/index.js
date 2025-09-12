aca0c0f2023487ad956dc53d730a53eaf04b7a5b
Ayush Chaurasiya <chaurasiyavinod96@gmail.com>
2025-09-12 22:07:25 +0530

    Fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -38,8 +38,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: true,       
-      sameSite: "none",   
+      secure: process.env.NODE_ENV === "production",      
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",  
     },
   })
 );
